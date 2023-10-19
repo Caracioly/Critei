a064e8a948f7dd05ea92ca3b80f342bd38062037
André Caracioly <120474277+Caracioly@users.noreply.github.com>
2023-10-19 16:45:35 -0300

    som implementado

--- Critei.lua
+++ Critei.lua
@@ -1,8 +1,12 @@
 local CritNotifier = CreateFrame('FRAME')
+local playerName = UnitName("player");
 
+-- CritNotifier:RegisterEvent('CHAT_MSG_SPELL_SELF_DAMAGE')
+CritNotifier:RegisterEvent('CHAT_MSG_YELL')
 CritNotifier:RegisterEvent('ADDON_LOADED')
 CritNotifier:RegisterEvent('UNIT_COMBAT')
 CritNotifier:SetScript("OnEvent", function()
+    -- When ADDON is loaded
     if event == "ADDON_LOADED" and arg1 == "Critei" then
         if HIGHEST_CRIT == nil then
             HIGHEST_CRIT = 0;
@@ -10,25 +14,57 @@ CritNotifier:SetScript("OnEvent", function()
         if HIGHEST_HEAL == nil then
             HIGHEST_HEAL = 0;
         end
-        DEFAULT_CHAT_FRAME:AddMessage("Addon loaded, HIGHEST_CRIT = " .. HIGHEST_CRIT)
-        DEFAULT_CHAT_FRAME:AddMessage("Addon loaded, HIGHEST_HEAL = " .. HIGHEST_HEAL)
     end
+
+    -- When you crital hit
     if event == "UNIT_COMBAT" and arg3 == "CRITICAL" and arg1 == 'target' then
         local msg = ""
+        local critAmount = arg4
+        local spellName = arg2
         if arg2 == 'HEAL' then
             if arg4 > HIGHEST_HEAL then
-                HIGHEST_HEAL = arg4
-                msg = "Critei " .. arg4 .. " de cura" -- heal case
+                HIGHEST_HEAL = critAmount
+                msg = "Critei " .. critAmount .. " de cura" -- heal case
+                PlaySoundFile("Interface\\AddOns\\Critei\\critSFX.ogg")
             end
         else
             if arg4 > HIGHEST_CRIT then
-                HIGHEST_CRIT = arg4
-                msg = "Critei " .. arg4 .. " de dano" -- damage case
+                HIGHEST_CRIT = critAmount
+                msg = "Critei " .. critAmount .. " de dano" -- damage case
+                PlaySoundFile("Interface\\AddOns\\Critei\\critSFX.ogg")
             end
         end
         SendChatMessage(msg, "YELL", nill)
     end
 
+    if event == 'CHAT_MSG_YELL' and arg2 ~= playerName then
+        if string.find(arg1, "Critei (%d+) de") then
+            PlaySoundFile("Interface\\AddOns\\Critei\\critSFX.ogg")
+        end
+    end
+
+    -- COMANDS --
+    -- Slash command to check your crit record
+    SLASH_CRIT1 = '/topcrit'
+    SlashCmdList["CRIT"] = function(msg)
+        print("Seu maior dano critico foi " .. HIGHEST_CRIT)
+    end
+
+    -- Slash command to check your heal record
+    SLASH_HEAL1 = "/topheal"
+    SlashCmdList["HEAL"] = function(msg)
+        print("Sua maior cura critica foi " .. HIGHEST_HEAL)
+    end
+
+    -- debug reset crit values
+    SLASH_CRESET1 = "/critreset"
+    SlashCmdList["CRESET"] = function(ms)
+        print("Resetando os valores criticos")
+        HIGHEST_CRIT = 0
+        HIGHEST_HEAL = 0
+    end
+
 end)
 
-
+-- SPELLCAST_STOP
+-- Fired when a spell cast stops. Called twice when the spell is channelled. Once at start, once at completion.
--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Critei - TWoW 1.12 addOn
 
-An simple way tho share your highest damage with your friends
+An simple way to share your highest damage with your friends
 - **Sends a Yell message when you beat you previously higher crit**
+- **Play VineBoom when you higher crit is beat**
 - **Also work with heals**
+
+`/topcrit -> print your current higher crit.`
+
+`/topheal -> print your current higher heal.`
 
 Note that due to limitations of the 1.12 API, behind the scenes, some cases is missing.<br/>
 
